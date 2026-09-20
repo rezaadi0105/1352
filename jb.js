49b15d9fd688cf993584a66eeb3e082a590e488c
rezaadi0105 <rezaadipangestu5@gmail.com>
2026-09-20 11:11:02 +0700

    slopkit: Updates

--- jb.js
+++ jb.js
@@ -1,4 +1,4 @@
-import { establishPrimitive } from "./core.js?v=10";
+import { establishPrimitive } from "./core.js";
 import { installWindowP, pairStatus } from "./mem.js";
 import { int64 } from "./int64.js";
 import { offsetsFor } from "./ps4_offsets.js";
